7fc481fafaebd5653922fe4aac2e5656562c9901
chu <1871361697@qq.com>
2016-08-12 23:08:00 +0800

    update

--- build_luajit.bat
+++ build_luajit.bat
@@ -1,5 +1,5 @@
 cd 3rdParty/luajit
 mkdir _build
 cd _build
-cmake .. -G "Visual Studio 12" -DLUAJIT_ENABLE_LUA52COMPAT=OFF
+cmake .. -G "Visual Studio 14" -DLUAJIT_ENABLE_LUA52COMPAT=OFF
 msbuild luajit.sln /t:Rebuild /p:Configuration=Release
